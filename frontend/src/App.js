ac8b5ae1913429a99b9b371663fee116094a0643
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-06 02:30:52 -0500

    Render home page template from index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
