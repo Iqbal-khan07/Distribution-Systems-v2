5c2c24fd3be6f68c0d2a4c05bc714b43d046a003
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-06 02:29:24 -0500

    Create home page template with styling

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import MenuBar from './components/MenuBar';
+import styles from './Home.module.css';
+
+const Home = () => {
+    return (
+        <div id="home-page" className={styles.content}>
+            <MenuBar />
+            <div className={styles.background}>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ frontend/src/pages/Home/components/MenuBar.jsx
@@ -0,0 +1,31 @@
+import { AppBar, Toolbar, Button} from '@material-ui/core';
+import styles from './MenuBar.module.css';
+import { StylesProvider } from '@material-ui/core/styles';
+
+
+const MenuBar = () => {
+    return (
+        <StylesProvider injectFirst>
+            <AppBar position='static' className={styles.menu}>
+                <Toolbar className={styles.menu}>
+                    <div className={styles.logo}>
+                        Commercial Distribution Solutions
+                        <img src='./CDSLogo.png' alt='CDS Logo' height='50'></img>
+                    </div>
+                    <div className={styles.grow}/>
+                    <Button className={styles.button}>
+                        About Us
+                    </Button>
+                    <Button className={styles.button}>
+                        Pricing
+                    </Button>
+                    <Button className={styles.login_button}>
+                        Log In
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </StylesProvider>
+    );
+}
+
+export default MenuBar;
