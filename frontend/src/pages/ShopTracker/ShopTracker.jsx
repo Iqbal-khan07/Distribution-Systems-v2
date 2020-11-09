b0e801f9d9a04f688d65bc98e71a3a7680cef91a
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-09 14:24:45 -0500

    Include shop list component in shop tracker page

--- frontend/src/pages/ShopTracker/ShopTracker.jsx
+++ frontend/src/pages/ShopTracker/ShopTracker.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import WithSignedInSkeleton from "../../shared/WithSignedInSkeleton/WithSignedInSkeleton";
+import ShopTable from "../../shared/ShopTable/ShopTable";
 
 const ShopTracker = () => {
     return (
         <WithSignedInSkeleton title={'Shop Tracker'}>
+        <ShopTable></ShopTable>
 
         </WithSignedInSkeleton>
     )
