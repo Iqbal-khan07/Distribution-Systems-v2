84c1a4f1cad8645f48a7bb08a638f28467856da2
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-12 18:58:52 -0500

    Allow page redirecting when signing in with google and facebook

--- frontend/src/shared/MainMenuBar/components/FacebookButton.jsx
+++ frontend/src/shared/MainMenuBar/components/FacebookButton.jsx
@@ -1,26 +1,31 @@
 import FacebookLogin from 'react-facebook-login';
- 
-const responseFacebook = (response) => {
-  console.log(response);
-  let email = response.email;
+import { useHistory } from "react-router-dom";
 
-  var xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', () => {
-    console.log(xhr.responseText)
-  })
-  //xhr.open('POST', 'https://')
-  //xhr.send(JSON.stringify({"authenticate_email":{"email":email}}))
-
-}
 
 export default function FacebookButton() {
-    return (
-        <FacebookLogin
-          appId="1064380820677269"
-          autoLoad={false}
-          fields="name,email,picture"
-          onClick=""
-          callback={responseFacebook}
-          isSignedIn={false} />
-      );
+  let history = useHistory();
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    let email = response.email;
+
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener('load', () => {
+      console.log(xhr.responseText)
+    })
+    //xhr.open('POST', 'https://')
+    //xhr.send(JSON.stringify({"authenticate_email":{"email":email}}))
+    history.push("/shoptracker");
+  }
+
+
+  return (
+    <FacebookLogin
+      appId="1064380820677269"
+      autoLoad={false}
+      fields="name,email,picture"
+      onClick=""
+      callback={responseFacebook}
+      isSignedIn={false} />
+  );
 }
--- frontend/src/shared/MainMenuBar/components/GoogleButton.jsx
+++ frontend/src/shared/MainMenuBar/components/GoogleButton.jsx
@@ -1,23 +1,28 @@
 import { GoogleLogin } from 'react-google-login';
+import { useHistory } from "react-router-dom";
 
-
-const responseGoogle = (response) => {
-  console.log(response);
-  let email = response.profileObj.email;
-
-  var xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', () => {
-    console.log(xhr.responseText)
-  })
-  //xhr.open('POST', 'https://')
-  //xhr.send(JSON.stringify({"authenticate_email":{"email":email}}))
-}
-
-function GoogleOAuthFailed(response) {
-  console.log("failed" + response);
-}
 
 export default function GoogleButton() {
+  let history = useHistory();
+  const responseGoogle = (response) => {
+    console.log(response);
+    let email = response.profileObj.email;
+
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener('load', () => {
+      console.log(xhr.responseText)
+    })
+    //xhr.open('POST', 'https://')
+    //xhr.send(JSON.stringify({"authenticate_email":{"email":email}}))
+    history.push("/shoptracker");
+  }
+
+  function GoogleOAuthFailed(response) {
+    console.log("failed" + response);
+  }
+
+
+
   return (
     <GoogleLogin
       clientId="892926001479-be1gj56ojqun7o89cfmkp39hpj7f020c.apps.googleusercontent.com"
