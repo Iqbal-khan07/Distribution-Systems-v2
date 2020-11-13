ac0ec2507c6ddc14852996ae600b654838d931fa
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-13 11:08:16 -0500

    Create invoice memo component for Order Page

--- frontend/src/pages/Orders/Orders.jsx
+++ frontend/src/pages/Orders/Orders.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import WithSignedInSkeleton from "../../shared/WithSignedInSkeleton/WithSignedInSkeleton";
 import ShowAddOrderFormButton from "./components/ShowAddOrderFormButton/ShowAddOrderFormButton";
 import OrderTable from "./components/OrderTable/OrderTable";
 import OrderForm from "./components/OrderForm/OrderForm";
+import OrderInfoPage from "./components/OrderInfoPage/OrderInfoPage";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -10,30 +11,30 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 
 const shopRows = [
-  createData(1001, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1002, 'Super Mart', '01/02/2020', '$150.00', '', 'Paid'),
-  createData(1003, 'Corner Central', '01/13/2020', '$400.00', '', 'Pending'),
-  createData(1004, 'General Plus', '03/01/2020', '$350.00', 'Leave with Manager', 'Paid'),
-  createData(1005, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1006, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1007, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1008, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1009, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1010, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1011, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1012, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1013, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
-  createData(1014, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1001, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1002, 'Super Mart', '01/02/2020', '$150.00', '', 'Paid'),
+    createData(1003, 'Corner Central', '01/13/2020', '$400.00', '', 'Pending'),
+    createData(1004, 'General Plus', '03/01/2020', '$350.00', 'Leave with Manager', 'Paid'),
+    createData(1005, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1006, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1007, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1008, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1009, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1010, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1011, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1012, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1013, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
+    createData(1014, 'ABC General Store', '01/01/2020', '$350.00', 'Leave with Manager', 'Delivered'),
 ];
 
 function createData(invoice, customer, date, amount, memo, status) {
-  return { invoice, customer, date, amount, memo, status };
+    return { invoice, customer, date, amount, memo, status };
 }
 
 const useStyles = makeStyles((theme) => ({
-  rootContainer: {
-      margin: "20px 0"
-  }
+    rootContainer: {
+        margin: "20px 0"
+    }
 }));
 
 const shopOptions = [
@@ -42,17 +43,34 @@ const shopOptions = [
         name: 'ABC General Store',
         zone: "Kunj"
     }
-]
+];
 
 const productOptions = [
-    {id:1, name: "Super Salted Crinkle-50", unitPrice: 45.00},
-    {id:2, name: "Super Salted Crinkle-51", unitPrice: 45.00},
-    {id:3, name: "Super Salted Crinkle-52", unitPrice: 45.00},
-]
+    { id: 1, name: "Super Salted Crinkle-50", unitPrice: 45.00 },
+    { id: 2, name: "Super Salted Crinkle-51", unitPrice: 45.00 },
+    { id: 3, name: "Super Salted Crinkle-52", unitPrice: 45.00 },
+];
 
+const orderDetails = {
+    id: 1001,
+    date: "01/01/2020",
+    shopId: 1140000,
+    pic: "",
+    shopName: "ABC General Store",
+    street: "123 Main Street",
+    city: "Anytown",
+    providence: "NJ",
+    zip: "07424",
+    phone: "800-555-5555",
+    email: null,
+    orderItems: [
+        { productName: "Super Salted Crinkle-50", productUnitPrice: 45.00, unitsOrdered: 8, productTotalCost: 45.00 }
+    ],
+    memo: ""
+};
 
 const Orders = () => {
-    const classes =useStyles();
+    const classes = useStyles();
     const [showOrderForm, setOrderForm] = useState(true)
     const [loading, setLoading] = useState(false)
 
@@ -85,6 +103,11 @@ const Orders = () => {
                                 </div>
                             </Grid>
                         </Grid>
+                        <Grid container spacing={3}>
+                            <Grid item lg={9} xs={9}>
+                                <OrderInfoPage details={orderDetails}></OrderInfoPage>
+                            </Grid>
+                        </Grid>
                     </div>
                     {showOrderForm ? (
                         <OrderForm
@@ -96,13 +119,13 @@ const Orders = () => {
                     ) : null}
                 </>
 
-            ): <CircularProgress />
+            ) : <CircularProgress />
 
             }
 
         </WithSignedInSkeleton>
 
     )
-}
+};
 
 export default Orders;
---
+++ frontend/src/pages/Orders/components/InvoiceTable/InvoiceTable.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+    table: {
+        minWidth: 700,
+    },
+    head: {
+        backgroundColor: "#232E33",
+        color: "#FFFFFF",
+    },
+    total: {
+        fontWeight: "bold",
+        color: "#5DB285",
+        paddingBottom: theme.spacing(10),
+    },
+    memo: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(5),
+    },
+}));
+
+
+export default function InvoiceTable({ orderItems }) {
+    const ccyFormat = (num) => (num.toFixed(2));
+    const priceRow = (qty, unit) => (qty * unit);
+
+    function createRow(desc, qty, unit) {
+        const price = priceRow(qty, unit);
+        return { desc, qty, unit, price };
+    }
+
+    function subtotal(items) {
+        return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+    }
+
+    const rows = orderItems.map((item) => {
+        return createRow(item.productName, item.unitsOrdered, item.productUnitPrice);
+    })
+
+    const invoiceSubtotal = subtotal(rows);
+    const deliveryFee = 0;
+    const invoiceTotal = deliveryFee + invoiceSubtotal;
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper} elevation="non">
+            <Table className={classes.table} size="small" aria-label="Invoice table">
+                <TableHead className={classes.head}>
+                    <TableRow>
+                        <TableCell className={classes.head}>Description</TableCell>
+                        <TableCell className={classes.head} align="right">Unit Price</TableCell>
+                        <TableCell className={classes.head} align="right">Qty.</TableCell>
+                        <TableCell className={classes.head} align="right">Sum</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.desc}>
+                            <TableCell>{row.desc}</TableCell>
+                            <TableCell align="right">{row.unit}</TableCell>
+                            <TableCell align="right">{row.qty}</TableCell>
+                            <TableCell align="right">{ccyFormat(row.price)}</TableCell>
+                        </TableRow>
+                    ))}
+
+                    <TableRow>
+                        <TableCell rowSpan={3} colSpan={2} />
+                        <TableCell>Subtotal</TableCell>
+                        <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>Delivery Fee</TableCell>
+                        <TableCell align="right">{ccyFormat(deliveryFee)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className={classes.total}>Grand Total</TableCell>
+                        <TableCell align="right" className={classes.total}>{ccyFormat(invoiceTotal)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell colSpan={4} className={classes.memo}>
+                            <b>Memo: </b>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
