8eb4865deb43c4567e948bf7272febcb9be1499b
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-28 16:18:49 -0500

    Create success/error alert when marking an order as delivered

--- frontend/src/pages/OrderFulfillerDashboard/components/DeliveryConfirmationScreen/DeliveryConfirmationScreen.jsx
+++ frontend/src/pages/OrderFulfillerDashboard/components/DeliveryConfirmationScreen/DeliveryConfirmationScreen.jsx
@@ -6,34 +6,76 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 
 export default function DeliveryConfirmationScreen(props) {
   /*const classes = useStyles();*/
   const { orderNumber, open, onClose, handleDisabled } = props;
-
+  const [confirmationAlertOpen, setConfirmationAlert] = React.useState(false);
+  const [errorAlertOpen, setErrorAlert] = React.useState(false);
   const handleClose = () => {
     onClose();
-    handleDisabled(true);
+  };
+  const handleConfirmationAlert = () => {
+    setConfirmationAlert(false);
+  };
+  const handleErrorAlert = () => {
+    setErrorAlert(false);
+  };
+
+  const handleConfirmation = async () => {
+    try {
+      await axios.post('/deliver/shop_order', {
+        data: {
+          shop_order_id: { orderNumber },
+          order_fulfiller_id: 0 //TODO
+        }
+      });
+      setConfirmationAlert(true);
+      handleDisabled(true);
+    }
+    catch (e) {
+      //TODO
+      setErrorAlert(true);
+    }
+    finally {
+      onClose();
+    }
   };
 
   return (
-    <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">{"Confirm Delivery?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Mark Order # {orderNumber} as delivered.
+    <div>
+      <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
+        <DialogTitle id="simple-dialog-title">{"Confirm Delivery?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Mark Order # {orderNumber} as delivered.
         </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="secondary">
-          Cancel
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
         </Button>
-        <Button onClick={handleClose} color="primary">
-          Confirm
+          <Button onClick={handleConfirmation} color="primary">
+            Confirm
         </Button>
-      </DialogActions>
-    </Dialog>
+        </DialogActions>
+      </Dialog>
+      <Snackbar open={confirmationAlertOpen} autoHideDuration={5000} onClose={handleConfirmationAlert}>
+        <MuiAlert elevation={6} variant="filled" onClose={handleConfirmationAlert} severity="success">
+          Order delivery confirmed!
+          </MuiAlert>
+      </Snackbar>
+      <Snackbar open={errorAlertOpen} autoHideDuration={5000} onClose={handleErrorAlert}>
+        <MuiAlert elevation={6} variant="filled" onClose={handleErrorAlert} severity="error">
+          Order delivery failed to confirm! Try again.
+          </MuiAlert>
+      </Snackbar>
+
+    </div>
   );
 }
 
