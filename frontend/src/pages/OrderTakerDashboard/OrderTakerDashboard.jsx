0acb07ed74255987ee495d3ab124a086fe34d7fe
Denisse <denisse@DESKTOP-8AT1CRD.localdomain>
2020-11-30 17:48:43 -0500

    Get info from backend

--- frontend/src/pages/OrderTakerDashboard/OrderTakerDashboard.jsx
+++ frontend/src/pages/OrderTakerDashboard/OrderTakerDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import WithSignedInSkeleton from "../../shared/WithSignedInSkeleton/WithSignedInSkeleton";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Grid from "@material-ui/core/Grid";
@@ -6,13 +6,40 @@ import InfoCard from "./components/InfoCard/InfoCard";
 import DataDisplayUtils from "../../utils/DataDisplayUtils";
 import CurrentVsTarget from "./components/CurrentVsTarget/CurrentVsTarget";
 import OrderByStatus from "./components/OrderByStatus/OrderByStatus";
+import { UserContext } from "../../context/UserContext"
 import axios from 'axios';
 
 export default function OrderTakerDashboard() {
-    const [loading, setLoading] = useState(false);
-    const series = [44, 55];
-    const series2 = [10, 2, 5]
+    const [loading, setLoading] = useState(true);
+    const [goalInfo, setGoalInfo] = useState({});
+    const { user } = useContext(UserContext);
+    const [salesSeries, setSalesSeries] = useState([44, 55]);
+    const [orderSeries, setOrderSeries] = useState([10, 2, 5]);
 
+    useEffect(() => {
+        async function fetchData() {
+            let response = await axios.post('/goal/order_taker', {
+                data: {
+                    order_taker_id: user.id
+                }
+            });
+            let body = response.data;
+
+            console.log(body);
+            const orderOptions = {
+                goal: body.data.goal_total,
+                current: body.data.current_value_total,
+                orders: body.data.num_orders_total,
+                orders_paid: body.data.orders_paid,
+                orders_pending: body.data.orders_pending
+            };
+            setGoalInfo(orderOptions);
+            setLoading(false);
+        }
+        fetchData().then()
+    }, [])
+
+    console.log(goalInfo);
     return (
         <WithSignedInSkeleton title={'Dashboard'}>
             {!loading ? (
@@ -22,18 +49,21 @@ export default function OrderTakerDashboard() {
                             <Grid container spacing={3}>
                                 <Grid item lg={4} xs={12}>
                                     <InfoCard
-                                        value={DataDisplayUtils.numberWithCommas(40000)}
+                                        value={DataDisplayUtils.numberWithCommas(goalInfo.goal)}
                                         description="Target for the Month"
                                     />
                                 </Grid>
                                 <Grid item lg={4} xs={12}>
                                     <InfoCard
-                                        value={DataDisplayUtils.numberWithCommas(21960)}
+                                        value={DataDisplayUtils.numberWithCommas(goalInfo.current)}
                                         description="Current Sales Value"
                                     />
                                 </Grid>
                                 <Grid item lg={4} xs={12}>
-                                    <InfoCard value={36} description="Orders Placed This Month" />
+                                    <InfoCard
+                                        value={DataDisplayUtils.numberWithCommas(goalInfo.orders)}
+                                        description="Orders Placed This Month"
+                                    />
                                 </Grid>
                             </Grid>
                         </Grid>
@@ -41,12 +71,12 @@ export default function OrderTakerDashboard() {
                             <Grid container spacing={3}>
                                 <Grid item lg={6} xs={12}>
                                     <CurrentVsTarget
-                                        series={series}
+                                        series={salesSeries}
                                     />
                                 </Grid>
                                 <Grid item lg={6} xs={12}>
                                     <OrderByStatus
-                                        series={series2}
+                                        series={orderSeries}
                                     />
                                 </Grid>
                             </Grid>
